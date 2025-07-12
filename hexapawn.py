be69a9a84a646304ed02ddadadfc6cec3484dab6
AnnalysaVicci <Annalysa.Vicci@gmail.com>
2025-07-12 05:42:52 -0400

    updates
    created to_board() function, got policy table built. Created network file

--- hexapawn.py
+++ hexapawn.py
@@ -9,16 +9,15 @@ class Hexapawn:
                       ['W','W','W']]
         #space 1 ahead is empty ->1
         #space 1 to the right or left has opponent ->1
-        
         self.player = 'W'
     
     #Function to transform board to vector representation
     def to_vector(self):
         vector_list = []
         if self.player == "W":
-            vector_list.append(0)
-        else:
             vector_list.append(1)
+        else:
+            vector_list.append(-1)
         for i in range(3):
             for j in range(3):
                 if self.board[i][j] == "W":
@@ -27,7 +26,15 @@ class Hexapawn:
                     vector_list.append(-1)
                 if self.board[i][j] == "-":
                     vector_list.append(0)
-        return vector_list
+        return vector_list 
+    
+    def to_board(self, state):
+        z = list(map(lambda x: 'W' if x == 1 else x, state))
+        z = list(map(lambda x: 'B' if x == -1 else x, z))
+        z = list(map(lambda x: '-' if x == 0 else x, z))
+        self.player = z[0]
+        b = [z[1:4],z[4:7],z[7:]]
+        self.board = b
 
     def change_turn(self):
         if self.player == 'B':
@@ -79,7 +86,6 @@ def possible_actions(s):
 def result(s,action):
     #should probably make a deep copy
     new_state = copy.deepcopy(s)
-
     r = action[1]
     c = action[2]
     if action[0] == 'Advance' and s.player=='W':
@@ -111,7 +117,6 @@ def is_terminal(s):
     #check if all pawns are captured
     if white_pawns == 0 or black_pawns == 0:
         return True
-
     #check if no moves left
     if not possible_actions(s):
         return True
@@ -138,7 +143,7 @@ def utility(s):
         
 def minimax(state):
     vm_pair = max_value(state)
-    print(vm_pair, 'vm pair')
+    print('vm_pair: ',vm_pair)
     return vm_pair[1]
 
 def max_value(state):
@@ -169,14 +174,12 @@ def policy_table_wrapper(init_state):
 def create_policy(state, action) -> list:
     new_state = copy.deepcopy(state)
     board = new_state.board
-
     position_i = action[1]
     position_j = action[2]
 
     for i in range(len(board)):
         for j in range(len(board)):
             board[i][j] = 0
-
     board[position_i][position_j] = 1
     new_state.board = board
     policy = []
@@ -193,7 +196,6 @@ def create_policy(state, action) -> list:
 def policy_table(state, table):
     if is_terminal(state):
         return table
-
     move = minimax(state)
     new_state = result(state, move)
     print(new_state.board,new_state.player)
@@ -214,6 +216,19 @@ def all_states(state):
             state_queue.append(result(s,action))
     return state_space
 
+def build_policy_table(game):
+    s = copy.deepcopy(game) 
+    curr = []
+    next = []
+    for state in all_states(game):
+        s.to_board(state)
+        action = minimax(s)
+        res = result(s,action)
+        curr.append(s.board)
+        #print('curr',curr)
+        next.append(res.board)
+        #print('next',next)
+    return curr,next
 
 #testing
 game = Hexapawn()
@@ -222,5 +237,10 @@ print(game.to_vector())
 #print(minimax(game))
 #the_policy_table = policy_table_wrapper(game)
 #print('The Policy Table: ', the_policy_table)
-for s in all_states(game):
-    print(s)
+#for s in all_states(game):
+    #print("all state: ",s)
+policy = {}
+x,y = build_policy_table(game)
+for i in range(len(x)):
+    policy[str(x[i])] = y[i]
+    print('curr: ', str(x[i]),' policy :',policy[str(x[i])])
