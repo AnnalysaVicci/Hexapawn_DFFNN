c3ffbf73df49720a23b6c3b1c21d4ef25b4a0d61
AnnalysaVicci <Annalysa.Vicci@gmail.com>
2025-07-12 06:08:35 -0400

    Update network.py
    developing layer and network class

--- network.py
+++ network.py
@@ -1,2 +1,24 @@
 #network.py
 
+#layer i of the network
+class Layer:
+    def __init__(self, parent, neurons, first_in):
+        self.parent = parent
+        self.n = neurons
+        self.m = parent.n
+        
+        #NxM matrix of weights
+        #N = num of neurons on layer i
+        #M = num of neurons on layer i-1
+        matrix = []
+
+class Network:
+    def __init__(self):
+        layers = []
+
+def classify(in_vector):
+    pass
+
+#Testing
+def main():
+    pass
